48efd0bbf6a28622341eace0bea73f00dbcde481
Yu Masuda <dkm17312@gmail.com>
2026-01-16 23:48:36 +0900

    modify Test.vue

--- frontend/src/components/Test.vue
+++ frontend/src/components/Test.vue
@@ -1,7 +1,10 @@
 <script setup></script>
-
+    import { ref } from 'vue';
+    const count = ref(0);
 <template>
     <div>
         <h1>Test Component</h1>
+        <p>Count: {{ count }}</p>
+        <button @click="count++">Increment</button>
     </div>
 </template>
